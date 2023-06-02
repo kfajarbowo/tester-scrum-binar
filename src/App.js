7777e210778b8451a607f837f8dd49aee39f8011
FajriAulia471 <fajriauliarachmandha@gmail.com>
2023-06-02 21:48:47 +0700

    add profile pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import Home from './pages/Home';
+import logo from "./logo.svg";
+import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" />
-				<Route path="/home" element={<Home />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
