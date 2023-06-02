91d6c7bf280bf0273cd9dc7b0936b52916a3ced3
yuyun1356 <124855983+yuyun1356@users.noreply.github.com>
2023-06-02 22:08:50 +0700

    add login (#3)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
+import logo from './logo.svg';
+import './App.css';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" />
-        <Route path="/home" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" />
+				<Route path="/home" element={<Home />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
